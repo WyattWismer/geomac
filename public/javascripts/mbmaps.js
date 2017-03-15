21cb0a838f0b48c2a762eb7ddf7fa0bf7894fea1
le-michael <zerglinglover@gmail.com>
2017-03-15 15:21:57 -0400

    Create base markers using data cordinates from a Json file

--- public/javascripts/mbmaps.js
+++ public/javascripts/mbmaps.js
@@ -1,3 +1,9 @@
+$.getJSON("public/javascripts/main_markers.json", function (json) {
+
+    mkdata = json;
+
+});
+
 $(document).ready(function () {
     //Declare variables
 
@@ -7,7 +13,7 @@ $(document).ready(function () {
     ];
 
     var out = [];
-    var mymap = L.map('map',{
+    var mymap = L.map('map', {
         maxBounds: bounds,
     }).setView([43.26158, -79.918], 16);
 
@@ -16,20 +22,27 @@ $(document).ready(function () {
 
     L.tileLayer('https://api.mapbox.com/styles/v1/le-michael/cj092gx3j002d2ro2bf40f1mi/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGUtbWljaGFlbCIsImEiOiJjajA4cG9wY3UwNzlzMnFwZWFmYTgyNG1yIn0.YAmObZ6YoXNa_O5tOJrZcg', {
         maxZoom: 20,
-        minZoom:15
+        minZoom: 15
     }).addTo(mymap);
 
     function onMapClick(e) {
         //console.log(e.latlng.lat);
+        //DEBUG LINE RIGHT HERE BOI GET RID OF IT
         console.log([e.latlng.lat, e.latlng.lng]);
     }
 
     //Declare Map Objects
-    var popup = L.popup()
-    .setLatLng([43.26158, -79.918])
-    .setContent('Welcome to geohack')
-    .openOn(mymap);
-
 
     mymap.on('click', onMapClick);
+
+    for (var i = 0; i < mkdata.Markers.Names.length; i++)
+        L.circle(mkdata.Markers.Cords[i], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 6
+        }).addTo(mymap).bindPopup(mkdata.Markers.Names[i]);
 });
+
+
+
