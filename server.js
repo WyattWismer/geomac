c4c7fd024898a6ff39d470db2340fc9db30fa66c
WyattWismer <crazy123frog@gmail.com>
2017-03-15 15:54:55 -0400

    build back end framework

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
-var express = require('express')
-var app = express()
+var firebase = require("firebase");
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-    res.sendFile('index.html', { root: __dirname } );
+
+app.get('/', function(req,res){
+	res.sendFile(__dirname + '/index.html')
 });
 
-app.use(express.static(__dirname ));
+io.on('connection', function(socket){
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+	
+	socket.on('marker request', function(){
+		
+		
+	});
+
+});
+
+
+http.listen(80, function(){
+	console.log('listening on *:80')
+});
+
+//firebase
+var config = {
+    apiKey: "AIzaSyAnoseJ4ZznNNyAEgH7aNf-X4RQZ_-n4bY",
+    authDomain: "testproject-146513.firebaseapp.com",
+    databaseURL: "https://testproject-146513.firebaseio.com",
+    storageBucket: "testproject-146513.appspot.com",
+    messagingSenderId: "589317577943"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
